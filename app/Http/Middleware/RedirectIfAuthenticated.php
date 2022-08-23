8eb0f892f73b0a84e69438af218a09e21739f7d5
Vinícius Tavolaro <viniciustavolaro@gmail.com>
2022-08-23 19:35:39 -0300

    correção redirect if authenticated

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -9,28 +9,15 @@ use Illuminate\Support\Facades\Auth;
 
 class RedirectIfAuthenticated
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
-     * @param  string|null  ...$guards
-     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
-     */
+
     public function handle(Request $request, Closure $next, ...$guards)
     {
         $guards = empty($guards) ? [null] : $guards;
 
         foreach ($guards as $guard) {
             if (Auth::guard($guard)->check()) {
-<<<<<<< HEAD
                 return redirect()->route('user.index');
-=======
-                return redirect(RouteServiceProvider::HOME);
->>>>>>> a875b7d438a38c2e34c4db4d1c2a95929cdcf116
             }
         }
-
-        return $next($request);
     }
 }
