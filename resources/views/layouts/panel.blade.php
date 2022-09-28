4888f91cb6ba95eaf264a2ae64599ef90a41d5d8
Vinícius Tavolaro <viniciustavolaro@gmail.com>
2022-09-28 01:31:46 -0300

    Melhorias na pagina do meet

--- resources/views/layouts/panel.blade.php
+++ resources/views/layouts/panel.blade.php
@@ -159,12 +159,11 @@
     </a>
 
     <!-- Bootstrap core JavaScript-->
-    <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
     <script src="{{ asset('vendor/bootstrap/bootstrap.bundle.min.js') }}"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 
     <!-- Custom scripts for all pages-->
     <script src="{{ asset('js/sb-admin-2.min.js') }}"></script>
-    <script src="{{ asset('vendor/jquery-mask/jquery.mask.min.js') }}"></script>
 
 </body>
 
--- resources/views/user/meets/meet.blade.php
+++ resources/views/user/meets/meet.blade.php
@@ -1,7 +1,4 @@
 @extends('layouts.panel')
-@section('title')
-    {{ $meet->name }}
-@endsection
 
 @section('content')
     @if (session()->has('success'))
@@ -15,7 +12,49 @@
             {{ session('warning') }}
         </div>
     @endif
-    <table class=" table mt-4 table-dark">
+    <div class="card shadow mb-4">
+        <div class="card-header py-3">
+            <h6 class="m-0 font-weight-bold text-primary">{{ $meet->name }}</h6>
+        </div>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                    <thead>
+                        <tr>
+                            <th class="text-center" colspan="{{$tamanho+1}}">Horários</th>
+                        </tr>
+                        <tr>
+                            <th scope="col">Participantes</th>
+
+                            @for ($i = 0, $tamanho = count($horarios); $i < $tamanho; ++$i)
+                                <th scope="col">
+
+                                <span>Data: </span>{{ $horarios[$i]->meet_date_formatted }} <span> | </span>
+                                <span>Início: </span>{{ $horarios[$i]->meet_start_formatted }} <span> | </span>
+                                <span>Fim: </span>{{ $horarios[$i]->meet_end_formatted }} <span> | </span>
+                                </th>
+                            @endfor
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($participants as $participant)
+                            <tr>
+                                <th class="dark" scope="row">
+                                    {{ $participant->name }}
+                                </th>
+                                @foreach ($horarios as $h)
+                                    <td scope="col" colspan="1">
+                                        <button id="voto" class="btn btn-danger btn-user btn-block"></button>
+                                    </td>
+                                @endforeach
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+    {{-- <table class=" table mt-4 table-dark">
         <thead class="thead-dark ">
 
             <tr class="">
@@ -58,25 +97,65 @@
                 </tr>
             @endforeach
         </tbody>
-    </table>
+    </table> --}}
 
-    <div class="row">
-        <div class="justify-content-between">
-            <a href="{{ route('horario.meet.create', $meet->id) }}" class="btn btn-primary">Novo Horario</a>
-        </div>
+    <div class="row ml-1">
+        <button class="btn btn-primary mr-2" type="button" data-toggle="collapse" data-target="#form-horario"
+            aria-expanded="false" aria-controls="form-horario">
+            Novo Horario
+        </button>
 
         <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#form-participant"
             aria-expanded="false" aria-controls="form-participant">
             Adicionar Particpante
         </button>
     </div>
+
     <div class="collapse" id="form-participant">
-        <div class="mt-1 card card-body">
+        <div class="mt-2 card card-body">
             <form class="form" action="{{ route('participant.store', $meet->id) }}" method="post">
                 @csrf
-                <input type="text" name="name" class="form-control" placeholder="Adicione o participante">
-                <button class="btn btn-primary" type="submit">Adicionar</button>
+                <input type="text" name="name" class="form-control mb-2" placeholder="Nome">
+                <button class="btn btn-primary btn-user btn-block" type="submit">Adicionar</button>
             </form>
         </div>
     </div>
+    <div class="collapse" id="form-horario">
+        <div class="mt-2 card card-body">
+            <form class="form" action="{{ route('horario.meet.store', $meet->id) }}" method="post">
+                @csrf
+                <div class="form-group">
+                    <label for="name">Data</label>
+                    <input type="text" name="meet_date"
+                        class="form-control date {{ $errors->has('meet_date') ? ' is-invalid' : '' }}"
+                        value="{{ old('meet_date') }}" data-mask="00/00/0000">
+                    <div class="invalid-feedback">{{ $errors->first('meet_date') }}</div>
+                </div>
+                <div class="form-group">
+                    <label for="name">Início</label>
+                    <input type="text" name="meet_start"
+                        class="form-control date {{ $errors->has('meet_start') ? ' is-invalid' : '' }}"
+                        value="{{ old('meet_start') }}" data-mask="00:00">
+                    <div class="invalid-feedback">{{ $errors->first('meet_start') }}</div>
+                </div>
+                <div class="form-group">
+                    <label for="name">Fim</label>
+                    <input type="text" name="meet_end"
+                        class="form-control date {{ $errors->has('meet_end') ? ' is-invalid' : '' }}"
+                        value="{{ old('meet_end') }}" data-mask="00:00">
+                    <div class="invalid-feedback">{{ $errors->first('meet_end') }}</div>
+                </div>
+                <button type="submit" class="btn btn-primary btn-user btn-block">
+                    Criar!
+                </button>
+            </form>
+        </div>
+    </div>
+
+    <script>
+        $("#voto").click(function() {
+            $(this).toggleClass('btn btn-success btn-user btn-block');
+        });
+
+    </script>
 @endsection
